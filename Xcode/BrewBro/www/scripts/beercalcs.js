d4106e9130f947634077fc1c76aa88715f418a9a
Robert Schmidt <rob@ayr.ca>
2012-06-04 11:42:55 -0400

    basic brewbrow in xcode

---
+++ Xcode/BrewBro/www/scripts/beercalcs.js
@@ -0,0 +1,332 @@
+/* Beercalcs script - an interesting collection of homebrewing-related
+ * calculations.
+ */
+
+function cToF(tempC) {
+	return (((tempC * 9) / 5) + 32);
+}
+
+function fToC(tempF) {
+	return ((5 * (tempF - 32)) / 9);
+}
+
+// this makes iterating functions cleaner:
+function cfTocf(unit, temp) {
+	if (unit == "C") {
+		return fToC(temp);
+	} else {
+		return cToF(temp);
+	}
+}
+
+function sgToPlato(SG) {
+	var plato = -616.989 + 1111.488 * SG - 630.606 * SG * SG + 136.10305 * SG
+			* SG * SG;
+	return plato;
+}
+
+function platoToSG(plato) {
+	var SG = 1.0000131 + 0.00386777 * plato + 1.27447E-5 * plato * plato
+			+ 6.34964E-8 * plato * plato * plato;
+	return SG;
+}
+
+function brixToSG(brix) {
+	var sg = 1.000898 + 0.003859118 * brix + 0.00001370735 * brix * brix
+			+ 0.00000003742517 * brix * brix * brix;
+	return sg;
+}
+
+function brixToFG(ob, fb) {
+	var fg = 1.001843 - 0.002318474 * ob - 0.000007775 * ob * ob - 0.000000034
+			* ob * ob * ob + 0.00574 * fb + 0.00003344 * fb * fb + 0.000000086
+			* fb * fb * fb;
+	return fg;
+}
+
+function SGBrixToABV(sg, fb) {
+	var ri = 1.33302 + 0.001427193 * fb + 0.000005791157 * fb * fb;
+	var abw = 1017.5596 - (277.4 * sg) + ri * ((937.8135 * ri) - 1805.1228);
+	var abv = abw * 1.25;
+	return abv;
+}
+
+/* yeast calcs */
+
+function yeastPitch(type, volml, sg) {
+
+	var plato = sgToPlato(sg);
+	var cells = 750000 * volml * plato;
+	if (type == 'lager')
+		cells = cells * 2;
+
+	var dryGr = cells / 17500000000;
+	cells = Math.round(cells / 1000000000);
+	return {
+		'cells' : cells,
+		'drygr' : dryGr
+	};
+}
+
+/* Carbonation calcs */
+function dissolvedCO2(BottleTemp) {
+	// returns the amount of dissolved CO2 in beer at BottleTemp
+	var DisCO2 = (1.266789 * BottleTemp)
+			+ 31.00342576
+			- (0.0000009243372 * (Math.sqrt((1898155717178 * Math.pow(
+					BottleTemp, 2))
+					+ 91762600000000
+					* BottleTemp
+					+ 839352900000000
+					- 1710565000000 * 14.5)));
+	return DisCO2;
+}
+
+function kegPSI(Temp, VolsCO2) {
+	// returns the PSI needed to carbonate beer at Temp at VolsCO2
+	PSI = -16.6999 - (0.0101059 * Temp) + (0.00116512 * Math.pow(Temp, 2))
+			+ (0.173354 * Temp * VolsCO2) + (4.24267 * VolsCO2)
+			- (0.0684226 * Math.pow(VolsCO2, 2));
+	return PSI;
+}
+
+sugars = {
+	'dextrose' : 1.15,
+	'honey' : 1.40,
+	'maplesyrup' : 1.50,
+	'molasses' : 1.8,
+	'DME' : 1.3,
+	'LME' : 1.4,
+	'brownsugar' : 1,
+	'tablesugar' : 1
+};
+function PrimingSugarGL(DisVolsCO2, TargetVolsCO2, SugarType) {
+	// returns the priming sugar in grams/litre needed to
+	// carbonate beer w/ a dissolved vols CO2 to reach the target vols CO2
+	// based on an article by Dave Draper in the July/August 1996 issue of
+	// Brewing Techniques.
+
+	var GramsPerLitre = (TargetVolsCO2 - DisVolsCO2) / 0.286;
+	GramsPerLitre *= sugars[SugarType];
+
+	return GramsPerLitre;
+}
+
+/* colour */
+function calcColour(lov, method) {
+	var colour = 0;
+	if (method == "ebc") {
+		// From Greg Noonan's article at
+		// http://brewingtechniques.com/bmg/noonan.html
+		colour = 1.4922 * Math.pow(lov, 0.6859); // SRM
+		// EBC is apr. SRM * 2.65 - 1.2
+		colour = (colour * 2.65) - 1.2;
+	} else {
+		// calculates SRM based on MCU (degrees LOV)
+		if (lov > 0)
+			colour = 1.4922 * Math.pow(lov, 0.6859);
+		else
+			colour = 0;
+	}
+
+	return colour;
+}
+
+function calcAlcohol(og, fg, method) {
+	var alc = 0;
+	if (!method) {
+		method = 'abv';
+	}
+	if (method == 'abw') {
+		var oPlato = sgToPlato(og);
+		var fPlato = sgToPlato(fg);
+		var q = 0.22 + 0.001 * oPlato;
+		var re = (q * oPlato + fPlato) / (1.0 + q);
+		alc = (oPlato - re) / (2.0665 - 0.010665 * oPlato);
+
+	} else {
+		alc = calcAlcohol(og, fg, 'abw') * fg / 0.794;
+	}
+	return alc;
+
+};
+
+/*
+ * Hop IBU calculation methods:
+ */
+function calcTinseth(amount, size, sg, time, aa, HopsUtil) {
+	var daautil; // decimal alpha acid utilization
+	var bigness; // bigness factor
+	var boil_fact; // boil time factor
+	var mgl_aaa; // mg/l of added alpha units
+	var ibu;
+
+	bigness = 1.65 * (Math.pow(0.000125, (sg - 1))); // 0.000125
+	// original
+	boil_fact = (1 - (Math.exp(-0.04 * time))) / HopsUtil;
+	daautil = bigness * boil_fact;
+	mgl_aaa = (aa / 100) * amount * 7490 / size;
+	ibu = daautil * mgl_aaa;
+	return ibu;
+};
+
+// rager method of ibu calculation
+// constant 7962 is corrected to 7490 as per hop faq
+function calcRager(amount, size, sg, time, AA) {
+	var ibu, utilization, ga;
+	utilization = 18.11 + 13.86 * rational_tanh((time - 31.32) / 18.27);
+	ga = sg < 1.050 ? 0.0 : ((sg - 1.050) / 0.2);
+	ibu = amount * (utilization / 100) * (AA / 100.0) * 7490;
+	ibu /= size * (1 + ga);
+	return ibu;
+};
+
+// utilization table for average floc yeast
+var util = [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 4, 5, 5, 6, 7, 9, 11, 13, 11, 13,
+		16, 13, 16, 19, 15, 19, 23, 16, 20, 24, 17, 21, 25 ];
+
+function calcGaretz(amount, size, sg, time, start_vol, yeast_flocc, AA) {
+	// iterative value seed - adjust to loop through value
+	var desired_ibu = calcRager(amount, size, sg, time, AA);
+	var elevation = 500; // elevation in feet - change for
+	// user setting
+	var concentration_f = size / start_vol;
+	var boil_gravity = (concentration_f * (sg - 1)) + 1;
+	var gravity_f = ((boil_gravity - 1.050) / 0.2) + 1;
+	var temp_f = (elevation / 550 * 0.02) + 1;
+
+	// iterative loop, uses desired_ibu to define hopping_f,
+	// then seeds
+	// itself
+	var hopping_f, utilization, combined_f;
+	var ibu = desired_ibu;
+	var util_index;
+	for ( var i = 0; i < 5; i++) { // iterate loop 5 times
+		hopping_f = ((concentration_f * desired_ibu) / 260) + 1;
+		if (time > 50)
+			util_index = 10;
+		else
+			util_index = Math.round(time / 5.0);
+		utilization = util[(util_index * 3) + yeast_flocc];
+		combined_f = gravity_f * temp_f * hopping_f;
+		ibu = (utilization * AA * amount * 0.749) / (size * combined_f);
+		desired_ibu = ibu;
+	}
+
+	return ibu;
+};
+
+function calcRGB(srm) {
+	var R, G, B;
+
+	if (srm < 11) {
+		R = 259.13 + (-7.42 * srm);
+		G = 278.87 + (-15.12 * srm);
+		B = 82.73 + (-4.48 * srm);
+	} else if (srm >= 11 && srm < 21) {
+		R = 335.27 + (-11.73 * srm);
+		G = 203.42 + (-7.58 * srm);
+		B = 92.50 + (-2.90 * srm);
+	} else {
+		R = 220.20 + (-7.20 * srm);
+		G = 109.42 + (-3.42 * srm);
+		B = 50.75 + (-1.33 * srm);
+	}
+
+	var r = Math.round(R);
+	var b = Math.round(B);
+	var g = Math.round(G);
+	if (r < 0)
+		r = 0;
+	if (r > 255)
+		r = 255;
+	if (g < 0)
+		g = 0;
+	if (g > 255)
+		g = 255;
+	if (b < 0)
+		b = 0;
+	if (b > 255)
+		b = 255;
+
+	return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+/*
+ * unit converters
+ */
+
+var weights = {
+	'g' : 453.292,
+	'oz' : 16,
+	'kg' : 0.453592,
+	'lb' : 1
+};
+
+function convertWgtTo(from, to, amount) {
+	return amount * weights[to] / weights[from];
+}
+
+var volumes = {
+	'gal' : 1,
+	'l' : 3.785411,
+	'qt' : 4,
+	'ml' : 3785.411,
+	'galimp' : 0.832674
+};
+
+function convertTo(from, to, amount) {
+	return amount * volumes[to] / volumes[from];
+};
+
+// hydrometer correction formula based on post by AJ DeLange in HBD 3701
+
+function deltaSG(TempC, SG) {
+	var coeffic = new Array();
+	coeffic[0] = [ 56.084, -0.17885, -0.13063 ]; // 0 - 4.99
+	coeffic[1] = [ 69.685, -1.367, -0.10621 ]; // 5 - 9.99
+	coeffic[2] = [ 77.782, -1.7288, -0.10822 ]; // 10 - 14.99
+	coeffic[3] = [ 87.895, -2.3601, -0.10285 ]; // 15 - 19.99
+	coeffic[4] = [ 97.052, -2.7729, -0.10596 ]; // 20 - 24.99
+	var plato = sgToPlato(SG);
+	// Default, > 20:
+	var coefficIndex = 4;
+	if (plato < 5)
+		coefficIndex = 0;
+	else if (plato < 10)
+		coefficIndex = 1;
+	else if (plato < 15)
+		coefficIndex = 2;
+	else if (plato < 20)
+		coefficIndex = 3;
+
+	var dSG = (coeffic[coefficIndex][0]) + (coeffic[coefficIndex][1] * TempC)
+			+ (coeffic[coefficIndex][2] * TempC * TempC);
+
+	// changed + to - from original
+	var correctedSG = platoToSG(plato - (dSG / 100));
+	return correctedSG;
+};
+
+function hydrometerCorrection(tempC, SG, refTempC) {
+
+	var correctedSG = 0;
+	if (refTempC == 20.0)
+		correctedSG = deltaSG(tempC, SG);
+	else
+		correctedSG = SG * deltaSG(tempC, SG) / deltaSG(refTempC, SG);
+
+	return correctedSG;
+}
+
+/*
+ * Utility Math functions not included in jscript math lib:
+ */
+function rational_tanh(x) {
+	if (x < -3)
+		return -1;
+	else if (x > 3)
+		return 1;
+	else
+		return x * (27 + x * x) / (27 + 9 * x * x);
+}
---
+++ Xcode/BrewBro/www/scripts/brewbro.js
@@ -0,0 +1,395 @@
+/* brewbro.js:
+ * Functions using jquery to handle change events on the brewbro UI.
+ * Supporting calculations are in beercalcs.js.
+ */
+
+// Set up onchange events.  jquery mobile uses doc.init() rather than document.ready()
+$('#water').live('pageinit', function(event) {
+	// profiles is defined in watercalcs.js:
+	for (p in profiles) {
+		$('#water_source').append($("<option />").val(p).text(p));
+		$('#water_target').append($("<option />").val(p).text(p));
+	}
+	;
+
+	// set each ion value
+	$('#water_source').change(function() {
+		var w = $('#water_source').val();
+		setIons(w, 'src');
+	});
+
+	$('#water_target').change(function() {
+		var w = $('#water_target').val();
+		setIons(w, 'trg');
+	});
+
+	// set both to distilled
+	$("#water_source :option first").attr('selected', 'selected');
+	$('#water_source').selectmenu("refresh", true);
+	$('#water_source').change();
+	$("#water_target :option first").attr('selected', 'selected');
+	$('#water_target').selectmenu("refresh", true);
+	$('#water_target').change();
+
+	// calculate
+	$('#calculate').click(function() {
+		var water_data = $('#water_form').serializeArray();
+
+		var src_ions = {};
+		var targ_ions = {};
+		jQuery.each(water_data, function() {
+			if (this.name.indexOf("_src") != -1) {
+				var k = this.name.substring(0, this.name.indexOf("_src"));
+				src_ions[k] = this.value;
+			} else if (this.name.indexOf("_trg") != -1) {
+				var k = this.name.substring(0, this.name.indexOf("_trg"));
+				targ_ions[k] = this.value;
+			}
+
+		});
+		// get vol and convert to gal
+		var vol = $('#water_vol').val();
+		var vol_u = $('#water_vol_u').val();
+		vol = convertTo(vol_u,'gal',vol);
+
+		var src_water = new water();
+		var targ_water = new water();
+		updateWater(src_water, src_ions);
+		updateWater(targ_water, targ_ions);
+		var res = calcWater(targ_water, src_water, vol);
+
+		// result is array of : treated water, tsp, dilution vol
+		var treat_water = res[0];
+		var salt_tsp = res[1];
+		var dil_vol = res[2];
+
+		for (ion in treat_water.treated) {
+			var t_id = '#' + ion + "_treat";
+			$(t_id).val(Math.round(treat_water.treated[ion]*10)/10);
+		}
+
+		for (salt in treat_water.salts){
+			var t_id='#'+salt;
+			$(t_id).val(Math.round(treat_water.salts[salt]*10)/10);
+		}
+		for (salt in salt_tsp) {
+			var salt_id = '#' + salt + "tsp";
+			$(salt_id).val(Math.round(salt_tsp[salt]*10)/10);
+		}
+
+		// convert dil vol from gal to target units:
+		dil_vol = convertTo('gal',vol_u,dil_vol);
+		$('#dilute_vol').val(Math.round(dil_vol*10)/10);
+
+		console.log(res);
+
+	});
+
+});
+
+// Call when the target or source drop-down changes:
+function setIons(profile, targ) {
+
+	for ( var key in profiles[profile]) {
+		var inp_id = '#' + key + "_" + targ;
+		$(inp_id).val(profiles[profile][key]);
+	}
+};
+
+$('#carb')
+		.live(
+				'pageinit',
+				function(event) {
+					/* Carbonation page change events */
+
+					/* change temp if units changes */
+
+					$('input:radio[name=carb_temp_u]')
+							.change(
+									function() {
+										var bottle_temp = $('#carb_bottletemp')
+												.val();
+										var serv_temp = $('#carb_serv_temp')
+												.val();
+										var tempU = $(
+												'input:radio[name=carb_temp_u]:checked')
+												.val();
+
+										if (tempU == 'c') {
+											bottle_temp = fToC(bottle_temp);
+											serv_temp = fToC(serv_temp);
+
+										} else {
+											bottle_temp = cToF(bottle_temp);
+											serv_temp = cToF(serv_temp);
+										}
+										$('#carb_bottletemp')
+												.val(
+														Math
+																.round(bottle_temp * 10) / 10);
+										$('#carb_serv_temp')
+												.val(
+														Math
+																.round(serv_temp * 10) / 10);
+
+									});
+
+					$(
+							'input:radio[name=carb_temp_u],#carb_targ_vols, #carb_serv_temp,#carb_sugar, #carb_beervol,#carb_beervol_u ')
+							.change(
+									function() {
+
+										var bottle_temp = $('#carb_bottletemp')
+												.val();
+										var serv_temp = $('#carb_serv_temp')
+												.val();
+										var tempU = $(
+												'input:radio[name=carb_temp_u]:checked')
+												.val();
+										if (tempU == 'c') {
+											bottle_temp = cToF(bottle_temp);
+											serv_temp = cToF(serv_temp);
+										}
+										var disolved_co2 = dissolvedCO2(bottle_temp);
+										$('#carb_co2')
+												.val(
+														Math
+																.round(disolved_co2 * 10) / 10);
+
+										var targ_vols = $('#carb_targ_vols')
+												.val();
+
+										var keg_psi = kegPSI(serv_temp,
+												targ_vols);
+										$('#carb_kegpsi').val(
+												Math.round(keg_psi * 10) / 10);
+
+										var sugar_type = $('#carb_sugar').val();
+										var beer_vol = $('#carb_beervol').val();
+										var beer_vol_u = $('#carb_beervol_u')
+												.val();
+
+										beer_vol = convertTo(beer_vol_u, 'l',
+												beer_vol);
+
+										var sugar_g_l = PrimingSugarGL(
+												disolved_co2, targ_vols,
+												sugar_type);
+										var sugar_add = sugar_g_l * beer_vol;
+										$('#carb_sugarg').val(
+												Math.round(sugar_add));
+
+									});
+
+				});
+
+$('#hydro').live('pageinit', function(event) {
+	/* Hyrdometer page change events */
+	/* change temp if units changes */
+	$('input:radio[name=hydr_tmp_u]').change(function() {
+		var temp = $('#hydr_tmp').val();
+		var ctemp = $('#callib_tmp').val();
+		var tempU = $('input:radio[name=hydr_tmp_u]:checked').val();
+
+		if (tempU == 'c') {
+			temp = fToC(temp);
+			ctemp = fToC(ctemp);
+
+		} else {
+			temp = cToF(temp);
+			ctemp = cToF(ctemp);
+		}
+		$('#hydr_tmp').val(Math.round(temp * 10) / 10);
+		$('#callib_tmp').val(Math.round(ctemp * 10) / 10);
+
+	});
+
+	$('#hydr_sg, #hydr_tmp, #callib_tmp').change(function() {
+		var sg = $('#hydr_sg').val();
+		var tempU = $('input:radio[name=hydr_tmp_u]:checked').val();
+		var temp = $('#hydr_tmp').val();
+		var ctemp = $('#callib_tmp').val();
+		if (tempU == 'f') {
+			temp = fToC(temp);
+			ctemp = fToC(ctemp);
+		}
+		var corrected_sg = hydrometerCorrection(temp, sg, ctemp);
+		$('#correct_sg').val(Math.round(corrected_sg * 1000) / 1000);
+
+	});
+});
+
+$('#yeast')
+		.live(
+				'pageinit',
+				function(event) {
+					/* Yeast page change events */
+					$(
+							'input:radio[name=rdo_yeasttype], #yeast_wortvol, #yeast_units, #yeast_og, #yeast_temp')
+							.change(
+									function() {
+										var type = $(
+												'input:radio[name=rdo_yeasttype]:checked')
+												.val();
+										var wortvol = $('#yeast_wortvol').val();
+										var units = $('#yeast_units').val();
+										var og = $('#yeast_og').val();
+
+										var volml = convertTo(units, 'ml',
+												wortvol);
+
+										var ret = yeastPitch(type, volml, og);
+										$('#yeast_count').val(ret['cells']);
+										$('#yeast_gr')
+												.val(
+														Math
+																.round(ret['drygr'] * 10) / 10);
+										$("#yeast_tubes")
+												.val(
+														Math
+																.round(ret['cells'] / 100 + 0.5));
+										$("#yeast_l")
+												.val(
+														Math
+																.round(ret['cells'] / 1.2) / 100);
+									});
+				});
+
+$('#refract').live('pageinit', function(event) {
+	/* Refractometer page change events */
+
+	$('#ref_abv_cbrix, #ref_abv_csg').change(function() {
+		var c_brix = $('#ref_abv_cbrix').val();
+		var c_csg = $('#ref_abv_csg').val();
+		var abv = SGBrixToABV(c_csg, c_brix);
+		$('#ref_abv_abv').val(Math.round(abv * 10) / 10);
+	});
+
+	$('#ref_og_brix, #ref_curr_brix').change(function() {
+		var og_brix = $('#ref_og_brix').val();
+		var curr_brix = $('#ref_curr_brix').val();
+		var sg = brixToFG(og_brix, curr_brix);
+		$('#ref_curr_sg').val(Math.round(sg * 1000) / 1000);
+	});
+
+	$('#ref_brix').change(function() {
+		var brix = $('#ref_brix').val();
+		var sg = brixToSG(brix);
+		$('#ref_sg').val(Math.round(sg * 1000) / 1000);
+	});
+});
+
+$('#convert').live('pageinit', function(event) {
+	/* Conversion page change events */
+	$("#in").change(function(event) {
+		calculate();
+	});
+
+	$("#convertmenu").change(function(event) {
+		var type = $("#convertmenu").val();
+		switch (type) {
+		case 'cToF':
+			$('#in-label').text('C:');
+			$('#outLabel').text('F:');
+			$("#in").attr({
+				min : '0',
+				max : '100',
+				step : '1'
+			});
+
+			$("#in").val('100');
+
+			break;
+
+		case 'fToC':
+			$('#in-label').text('F:');
+			$('#outLabel').text('C:');
+			$("#in").attr({
+				min : '0',
+				max : '212',
+				step : '1'
+			});
+			$("#in").val('212');
+			break;
+
+		case 'sgToPlato':
+			$('#in-label').text('SG:');
+			$('#outLabel').text('Plato:');
+			$("#in").val('1.050');
+			$("#in").attr({
+				min : '1.000',
+				max : '1.130',
+				step : '0.001'
+			});
+			break;
+
+		case 'platoToSg':
+			$('#in-label').text('Plato:');
+			$('#outLabel').text('SG:');
+			$("#in").attr({
+				min : '0',
+				max : '25',
+				step : '0.5'
+			});
+			$("#in").val('12');
+			break;
+
+		case 'brixToSG':
+			$('#in-label').text('Brix:');
+			$('#outLabel').text('SG:');
+			$("#in").val('12');
+			$("#in").attr({
+				min : '0',
+				max : '25',
+				step : '0.5'
+			});
+			break;
+
+		default:
+			alert("no luck");
+			break;
+		}
+		calculate();
+	});
+});
+
+/* Conversion page support functions */
+function calculate() {
+	type = $('#convertmenu').val();
+	inp = $("#in").val();
+	var out = 0;
+	switch (type) {
+	case 'cToF':
+		out = cToF(inp);
+		out = Math.round(out * 10) / 10;
+		break;
+
+	case 'fToC':
+		out = fToC(inp);
+		out = Math.round(out * 10) / 10;
+		break;
+
+	case 'sgToPlato':
+		out = sgToPlato(inp);
+		out = Math.round(out * 10) / 10;
+		break;
+
+	case 'platoToSg':
+		out = platoToSG(inp);
+		out = Math.round(out * 1000) / 1000;
+		break;
+
+	case 'brixToSG':
+		out = brixToSG(inp);
+		out = Math.round(out * 1000) / 1000;
+		break;
+
+	default:
+		alert("no luck");
+		break;
+
+	}
+	// alert(out);
+
+	$("#out").val(out);
+}
